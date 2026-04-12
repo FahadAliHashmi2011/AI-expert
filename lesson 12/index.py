b0878855247f56ea0df29382c84ee907ff8d1994
FahadAliHashmi2011 <faiezali2006@gmail>
2026-04-12 16:39:50 +0100

    l12.done

---
+++ lesson 12/index.py
@@ -0,0 +1,40 @@
+import cv2
+
+face_cascade = cv2.CascadeClassifier(
+    cv2.data.haarcascades +"haarcascade_frontalface_default.xml"
+)
+
+cap = cv2.VideoCapture(0)
+if not cap.isOpened():
+    print("error: camera is not working")
+    exit()
+while True :
+    success,frame =cap.read()
+    if not success:
+        print("failed to grab frame")
+        break
+    gray = cv2.cvtColor(frame,cv2.COLOR_BGR2RGB)
+    faces = face_cascade.detectMultiScale(
+        gray,
+        scaleFactor=1.1,
+        minNeighbors=5,
+        minSize=(30,30)
+    )
+
+    for(x,y,w,h,) in faces :
+        cv2.rectangle(
+            frame,
+            (x,y),
+            (x+w,y+h),
+            (255,0,0),
+            2
+        )
+
+    cv2.imshow("face detction(press q to quit)",frame)
+    if cv2.waitKey(1) & 0xFF == ord("q") :
+        break
+cap.release()
+cv2.destroyAllWindows()
+
+
+
